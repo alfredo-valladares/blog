23407c90aceb2cd21c4fdc3202bf8bc7d4bd1f7c
alfredo-valladares <josealfredo.developer@outlook.com>
2020-11-26 13:23:18 -0600

    first commit

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
+
+class PaymentController extends Controller
+{
+    //
+
+
+
+
+
+
+
+
+
+
+}
